6fa7bf5e682c76ef0788c6dd6901809dcfab27b1
Santiago Darío Franco Ruiz <102566762+francosantiago@users.noreply.github.com>
2022-11-17 08:34:50 -0500

    francosantiago

--- ejecutable/test.java
+++ ejecutable/test.java
@@ -1,6 +1,28 @@
 package ejecutable;
 
+import javax.swing.JOptionPane;
+
+import modelo.circulo;
+import modelo.rectangulo;
+
 public class test 
 {
-    
+    public static void main(String[] args) {
+        // Input
+        double x = Integer.parseInt(JOptionPane.showInputDialog("Digite el primer lado del rectángulo: "));
+        double y = Integer.parseInt(JOptionPane.showInputDialog("Digite el segundo lado del rectángulo: "));
+
+        double z = Integer.parseInt(JOptionPane.showInputDialog("Digite el radio del círculo: "));
+
+        // Processing
+        rectangulo miRectangulo = new rectangulo(x, y);
+        JOptionPane.showMessageDialog(null, "El perimetro del rectángulo es: "+ miRectangulo.calcularPerimetroR(), "Perimetro", 1);
+        JOptionPane.showMessageDialog(null, "El área del rectángulo es: "+ miRectangulo.areaR(), "Area", 1);
+
+        circulo miCirculo = new circulo(z);
+        JOptionPane.showMessageDialog(null, "El perimetro del círculo es: "+ miCirculo.calcularPerimetroC(), "Perimetro", 1);
+        JOptionPane.showMessageDialog(null, "El área del círculo es: "+ miCirculo.area(), "Area", 1);
+
+        System.exit(0);
+    }
 }
